b6124cc43bbb7c985f418cbc06042c41eda97f29
chris <chlds@yahoo.com>
2025-06-07 22:24:55 +0900

    Update

---
+++ docs/js/g.js
@@ -0,0 +1,14 @@
+function g(...arg) {
+var r;
+const b = ("Building a library in C");
+const w = (`Spring-Summer 2025`);
+r = document.getElementById("title");
+r.textContent = (b);
+r = document.getElementById("masthead");
+r.textContent = (b);
+r = document.getElementById("byline");
+r.textContent = (w);
+return(0x00);
+}
+
+document.onload = g(~0x00);
---
+++ docs/js/p.js
@@ -0,0 +1,9 @@
+function g(...arg) {
+var r;
+const b = ("Currently under development");
+r = document.getElementById("lead");
+r.textContent = (b);
+return(0x00);
+}
+
+document.onload = g(~0x00);
